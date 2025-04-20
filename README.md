f2af1046fd209da3b20caf69bdf285efbf869e54
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-04-20 14:57:43 +0530

    stable

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # a2v
-a2v
+
+a2v is a simple script(that uses ffmpeg under the hood) to convert mp3/m4a audio file to mp4 video.
+
+## Dependencies
+
+- os: linux mint
+- bash
+- rust toolchain
+- ffmpeg cli
+
+## To use
+
+1. clone this repo
+2. cd into a2v directory
+3. run: `cargo run`
+4. enter the fields
+5. done!
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, process::exit};
+use std::{env::args, process::{exit, Command}};
 
 const VERSION:&str = "0.1.0";
 
@@ -12,7 +12,7 @@ fn main() {
     println!("a2v {VERSION}");
     println!("A simple tool (that uses ffmpeg under the hood)\nto convert mp3 audio to mp4 video.");
     loop {
-        println!("Enter mp3 file name:");
+        println!("Enter mp3/m4a file name:");
         let mp3_file = input();
         if mp3_file.is_empty() {
             continue;
@@ -37,7 +37,59 @@ fn input() -> String {
 }
 
 fn a2v(mp3_file_name:&str, mp4_file_name:&str, image_file_name:&str){
-    dbg!(mp3_file_name);
-    dbg!(mp4_file_name);
-    dbg!(image_file_name);
+    if !mp3_file_name.ends_with(".mp3") && !mp3_file_name.ends_with(".m4a") {
+        println!("Error: audio file name must end with .mp3/m4a");
+        return;
+        
+    }
+    if !mp4_file_name.ends_with(".mp4") {
+        println!("Error: mp4 file name must end with .mp4");
+        return;
+    }
+
+
+
+    if image_file_name.is_empty()  {
+        let mut ffmpeg = Command::new("sh");
+        
+            match ffmpeg.arg("-c")
+            .arg(format!("ffmpeg -i {mp3_file_name} -vn -c:a copy {mp4_file_name}"))
+            .output(){
+                Ok(output) => {
+                    if output.status.success() {
+                        println!("Successfully converted:\n {} -> {}", mp3_file_name, mp4_file_name);
+                        exit(0);
+                    } else {
+                        println!("error while converting!");
+                        exit(1);
+                    }
+                }
+                Err(_) => {
+                    println!("internal error!");
+                    exit(1);
+                }
+            }
+    }else {
+        let mut ffmpeg = Command::new("sh");
+
+        match ffmpeg.arg("-c")
+          .arg(format!("ffmpeg -loop 1 -i {image_file_name} -i {mp3_file_name} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest {mp4_file_name}")).output() {
+            Ok(output) => {
+                if output.status.success() {
+                    println!("Successfully converted:\n {} -> {}", mp3_file_name, mp4_file_name);
+                    exit(0);
+                } else {
+                    dbg!(&output);
+                    println!("error while converting!");
+                    exit(1);
+                }
+            }
+            Err(_) => {
+                println!("internal error!");
+                exit(1);
+            }    
+        }
+
+        
+    }
 }
